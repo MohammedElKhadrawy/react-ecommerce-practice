d51f71c4d8a46fe39142829a08de03a73e93f218
MohammedElKhadrawy <mohammedelkhadrawy641@gmail.com>
2024-08-31 14:37:59 +0300

    added persistance and rehydration for cart items using redux-persist

--- Front-End/src/store/index.ts
+++ Front-End/src/store/index.ts
@@ -1,16 +1,46 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import categoriesReducer from './categories/categoriesSlice';
 import productsReducer from './products/productsSlice';
 import cartReducer from './cart/cartSlice';
 
-export const store = configureStore({
-  reducer: {
-    categories: categoriesReducer,
-    products: productsReducer,
-    cart: cartReducer,
-  },
+// Nested persist
+const cartPersistConfig = {
+  key: 'cart',
+  storage,
+  whitelist: ['items'],
+};
+
+const rootReducer = combineReducers({
+  categories: categoriesReducer,
+  products: productsReducer,
+  cart: persistReducer(cartPersistConfig, cartReducer),
 });
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
